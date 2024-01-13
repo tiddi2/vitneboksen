5334d78ef0aa725d9799e50446193140e8051a87
Mats Løvstrand Berntsen <matslovstrandberntsen@gmail.com>
2024-01-13 16:12:40 +0100

    removing footer

--- App/src/components/Home/home.js
+++ App/src/components/Home/home.js
@@ -498,6 +498,79 @@ const Home = () => {
           )}
         </div>
       )}
+      <div
+        className="footer"
+        style={{
+          position: "fixed",
+          display: "flex",
+          bottom: "0",
+          left: "5%",
+          right: "5%",
+        }}
+      >
+        <p
+          style={{
+            bottom: "0",
+            left: "5%",
+            alignItems: "baseline",
+            alignContent: "baseline",
+          }}
+        >
+          Sponset av
+          <a href="https://spritjakt.no">
+            <img
+              src="spritjakt.png"
+              alt="spritjakt logo"
+              style={{ margin: "5px" }}
+              height={"25px"}
+            />
+          </a>
+          og
+          <a href="https://erdetfesthosmatsikveld.no">
+            erdetfesthosmatsikveld.no
+          </a>
+        </p>
+        <p
+          style={{
+            right: "0%",
+            left: "0%",
+            bottom: "0",
+            marginLeft: "auto",
+            marginRight: "auto",
+            alignItems: "baseline",
+            alignContent: "baseline",
+            gap: "10px",
+          }}
+        >
+          © 2024{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://no.linkedin.com/in/mats-l%C3%B8vstrand-berntsen-4682b2142"
+          >
+            Mats Løvstrand Berntsen
+          </a>
+        </p>
+        <p
+          style={{
+            display: "flex",
+            bottom: "0",
+            right: "5%",
+            alignItems: "baseline",
+            alignContent: "baseline",
+            gap: "10px",
+          }}
+        >
+          Kildekoden finner du på
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/matslb/vitneboksen"
+          >
+            Github
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
