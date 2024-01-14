7631365dcf5a049602d36623a7e958d1d14dea45
Mats Løvstrand Berntsen <matslovstrandberntsen@gmail.com>
2024-01-14 21:58:01 +0100

    gitignore

--- Api/Vitneboksen_func/Vitneboksen_func/CreateSession.cs
+++ Api/Vitneboksen_func/Vitneboksen_func/CreateSession.cs
@@ -1,48 +1,50 @@
+
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace Vitneboksen_func
 {
-    using Azure.Storage.Blobs;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.Azure.WebJobs;
-    using Microsoft.Azure.WebJobs.Extensions.Http;
-    using Microsoft.Extensions.Logging;
-    using System;
-    using System.Linq;
-    using System.Threading.Tasks;
-
-    namespace Vitneboksen_func
+    public static class CreateSession
     {
-        public static class CreateSession
+        [FunctionName("get-session")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
         {
-            [FunctionName("create-session")]
-            public static async Task<IActionResult> Run(
-                [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-                ILogger log)
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var constring = Environment.GetEnvironmentVariable("StorageConnectionString");
+            var blobService = new BlobServiceClient(constring);
+
+            var sessionKey = req.Query["sessionKey"];
+            string sharingKey;
+
+            var containerClient = Helpers.GetContainerBySessionKey(blobService, sessionKey);
+            if (containerClient == null)
             {
-                log.LogInformation("C# HTTP trigger function processed a request.");
-
-                var constring = Environment.GetEnvironmentVariable("StorageConnectionString");
-                var blobService = new BlobServiceClient(constring);
-
-                var sessionKey = req.Query["sessionKey"];
-                string sharingKey;
-
-                var containerClient = Helpers.GetContainerBySessionKey(blobService, sessionKey);
-                if (containerClient == null)
-                {
-                    sharingKey = Guid.NewGuid().ToString().Substring(0, 8);
-                    containerClient = blobService.GetBlobContainerClient($"{sessionKey}-{sharingKey}");
-                    await containerClient.CreateAsync();
-                }
-                else
-                {
-                    sharingKey = containerClient.Name.Split("-").Last();
-                }
-
-                return new OkObjectResult(sharingKey);
+                sharingKey = Guid.NewGuid().ToString().Substring(0, 8);
+                containerClient = blobService.GetBlobContainerClient($"{sessionKey}-{sharingKey}");
+                await containerClient.CreateAsync();
+            }
+            else
+            {
+                sharingKey = containerClient.Name.Split("-").Last();
             }
 
+            var blobs = containerClient.GetBlobs();
+            var videoCount = blobs.Count(b => b.Name.Contains(".mp4") || b.Name.Contains(".webm"));
+            var latestUploadTime = blobs.MaxBy(b => b.Properties.CreatedOn)?.Properties.CreatedOn;
+            return new OkObjectResult(new SessionStatus(sessionKey, sharingKey, videoCount, latestUploadTime));
         }
     }
 
+    public record SessionStatus(string SessionKey, string SharingKey, int VideoCount, DateTimeOffset? LastUpload);
 }
+
