738cd02413d3eed1afe23ef15687378a7efa5104
Mats Løvstrand Berntsen <68021290+matslb@users.noreply.github.com>
2024-01-29 10:09:18 +0100

    Update UploadActionShot.cs

--- Api/Vinteboksen_Api/Controllers/UploadActionShot.cs
+++ Api/Vinteboksen_Api/Controllers/UploadActionShot.cs
@@ -36,8 +36,8 @@ public static class UploadActionShot
             var outputFilePath = Path.Combine(tempPath, $"{DateTime.Now.ToFileTimeUtc()}.mp4");
 
             var ffmpegCmd = OperatingSystem.IsWindows() ?
-            $"-i \"{videoFilePath}\" -c:v copy -c:a copy \"{outputFilePath}\""
-            : $"-i \"{videoFilePath}\" -c:v copy -c:a copy \"{outputFilePath}\"";
+            $"-i \"{videoFilePath}\" -c:v libx264 -c:a aac \"{outputFilePath}\""
+            : $"-i \"{videoFilePath}\" -c:v libx264 -c:a aac \"{outputFilePath}\"";
 
             await Helpers.ExecuteFFmpegCommand(ffmpegCmd);
 
